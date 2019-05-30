758f02395434edb4e8af9dc0f44ec2f5d844e35c
cc <cc@192.168.127.62>
2019-05-30 16:02:30 +0800

    1

--- test3/src/test3/Test3.java
+++ test3/src/test3/Test3.java
@@ -4,6 +4,8 @@ public class Test3 {
 	  public static void main(String[] args) {
 		System.out.println("111");
 		System.out.println("aaabbb");
+		System.out.println("cccddd");
 		System.out.println("xxxyyy");
+		
 	}
 }
